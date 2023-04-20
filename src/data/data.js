c127a68c489083a0730b8586ce6a9700dda8ba87
Ayoub Basidi <93930160+AYOUBBASIDI@users.noreply.github.com>
2023-04-20 15:30:41 +0000

    ©️

---
+++ src/data/data.js
@@ -0,0 +1,9 @@
+const months = [ 'Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec' ];
+const monthsNames = [ 'January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December' ];
+const daysName = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+
+module.exports = {
+    months,
+    monthsNames,
+    daysName
+}
